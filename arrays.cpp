c6a47a07dab5648f6b2943d3b4aa233cdf36aa81
hiiambhanu <hiiambhanu@gmail.com>
2019-08-17 14:15:37 +0530

    linear and binary search

--- arrays.cpp
+++ arrays.cpp
@@ -9,6 +9,42 @@ void swap(int &a, int&b){
 
 }
 
+void lsearch(int a[], int size, int item){
+    for(int i = 0 ;i < size; i++)
+        if(a[i]==item){
+            cout<<"Item Found at "<<i<<" ";
+            return;
+        }
+    cout<<"Item not found";
+    return;
+}
+
+void bsearch(int a[], int size, int item){
+    int lb, ub, mid;
+    lb = 0; 
+    ub = size-1;
+    
+    while(lb <= ub){
+        
+        mid = (lb + ub) /2;
+
+        if(a[mid]==item){
+            cout<<"Item found ";
+            return;
+        }
+
+        else if(a[mid]>item ){
+            ub = mid - 1; 
+        }
+
+        else lb = mid + 1;  
+    }
+
+    cout<<"Item not found";
+    return;
+    
+}
+
 void bsort(int a[], int size)
 {
     for(int i=0;i<size;i++){
@@ -30,10 +66,14 @@ int main()
     cout<<"Enter the array elements ";
     for(int i = 0;i < size; i++)
         cin>>a[i];
-    bsort(a, size);
-    cout<<"\n the sorted array is "<<endl;
+    
+    cout<<"Enter the item to look for ";
+    int item;
+    cin>>item;
+    cout<<"bSearch "; 
+    bsearch(a, size, item);
 
-    display(a, size);
+
     return 0; 
 
 }
